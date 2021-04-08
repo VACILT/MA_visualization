853cb7e78156e2e997c84ea363ea6302cd2ee939
Willy Stöckel <ws95hero@studserv.uni-leipzig.de>
2021-04-08 16:28:33 +0200

    first attempt implemtenting 2d-plot

--- code/radar_wind.py
+++ code/radar_wind.py
@@ -21,8 +21,37 @@ import matplotlib.pyplot as plt
 root_path = '/project/MA_vis/MA_visualization/data/'
 
 ds_info = xr.open_dataset(f'{root_path}MR_wind_2021_01_07.h5', group = 'info')
-ds_info['alt']
+#ds_info
 
-ds_test = xr.open_dataset(f'{root_path}MR_wind_2021_01_07.h5')
+# ## Getting Metadata
+
+year = int(ds_info['date'].values[0,0])
+month =int(ds_info['date'].values[1,0])
+day =int(ds_info['date'].values[2,0])
+date = str(day)+"."+str(month)+"."+str(year)
+date
+
+ds_wind = xr.open_dataset(f'{root_path}MR_wind_2021_01_07.h5', group = 'wind')
+ds_wind
+
+ds_wind['u']
+ds_wind.rename({'phony_dim_6': 'alt', 'phony_dim_7': 'time'})
+ds_wind['alt'] = ds_info['alt'].squeeze().values
+ds_wind['alt'].attrs['long_name'] = 'altitude'
+ds_wind['alt'].attrs['units'] = 'km'
+
+ds_wind['u'].plot(x= 'phony_dim_6', y = 'phony_dim_7')
+
+# ## 2d plot
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ds_wind_sel = ds_wind.isel(phony_dim_6=0)
+ds_wind_sel['u'].plot(y = 'phony_dim_7', label='u')
+ds_wind_sel['v'].plot(y = 'phony_dim_7', label='v')
+#ds_wind_sel['w'].plot(y = 'phony_dim_7')
+ax.set_title('Collm zonal and merdional wind on '+ date)
+plt.legend()
+plt.show()
 
 
