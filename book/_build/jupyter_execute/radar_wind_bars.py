e825bb526302c17707daaed058cf31b05933384d
Willy Stöckel <ws95hero@studserv.uni-leipzig.de>
2021-05-27 17:26:43 +0200

    update book

--- book/_build/jupyter_execute/radar_wind_bars.py
+++ book/_build/jupyter_execute/radar_wind_bars.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Zonal and meridional wind by altidude
+# 
+# Use the tools provided in the toolbar to drag, zoom or save the plot as pdf for further use.
+
 # In[1]:
 
 
@@ -16,67 +20,54 @@ import numpy as np
 # In[2]:
 
 
-#root_path = '/projekt1/radar/webtool/'
 root_path = '/project/MA_vis/MA_visualization/data/'
 
 
 # In[3]:
 
 
+root_path = '/project/MA_vis/MA_visualization/data/'
 cascade_infiles = sorted(glob.glob(f'{root_path}*.h5'))
-ds_wind = xr.open_mfdataset(cascade_infiles[0:11], concat_dim=['date'],group ='wind', combine = 'nested')
+ds_wind = xr.open_mfdataset(cascade_infiles[0:11], concat_dim=['phony_dim_7'],group ='wind', combine = 'nested')
 ds_info = xr.open_dataset(cascade_infiles[0], group = 'info')
 dates=[]
 for i in cascade_infiles:
     dates.append(i[-13:-3])
-ds_wind['date'] = dates[0:11]
-
-
-# ## Prepare Dateset
-
-# In[4]:
-
-
-#ds_wind = xr.open_dataset(cascade_infiles[i_file], group = 'wind')
-#ds_tides= xr.open_dataset(cascade_infiles[i_file], group = 'tides')
-#ds_wind
-
-
-# In[5]:
-
 
 ds_wind = ds_wind.rename({'phony_dim_7': 'time', 'phony_dim_8': 'alt'})
 ds_wind['alt'] = ds_info['alt'].squeeze().values
 ds_wind['alt'].attrs['long_name'] = 'altitude'
 ds_wind['alt'].attrs['units'] = 'km'
-ds_wind['time'].attrs['units'] = 'h'
-ds_wind['time']=np.arange(1,25,1)
-#ds_wind
+ds_wind['time']=np.arange(0,11,1/24)
+ds_wind['time'].attrs['units'] = f'days after {dates[0]}'
+
+
+# In[4]:
+
+
+def bars(alti):
+    bars = ds_wind.sel(alt=alti).hvplot.scatter(y=['u','v'], symmetric =True, ylim=[-100,100])     *ds_wind.sel(alt=alti).hvplot.errorbars(y='u', yerr1='u_err')     *ds_wind.sel(alt=alti).hvplot.errorbars(y='v', yerr1='v_err').opts(title=f'zonal (u) and meridional (v) flow in {alti} km')
+    return (bars)
+
+
+# In[5]:
+
+
+gspec = pn.GridSpec(width=800, height=600, margin=5)
+gspec[0:1, 0] = bars(80)
+gspec[1:2, 0] = bars(90)
+gspec[2:3, 0] = bars(100)
+gspec
 
 
 # In[6]:
 
 
-hvc_opts = dict(x = 'time', y = 'alt')
-
-con_err_v = ds_wind['v_err'].hvplot.contour(**hvc_opts)
-con_err_u = ds_wind['u_err'].hvplot.contour(**hvc_opts)
-
-
-# In[7]:
-
-
-bars = ds_wind.hvplot.scatter(y=['u','v'], symmetric =True, hover=False, ylim=[-100,100]) *ds_wind.hvplot.errorbars(y='u', yerr1='u_err') *ds_wind.hvplot.errorbars(y='v', yerr1='v_err').opts(toolbar=None)
-
-
-# In[8]:
-
-
-bars
-
-
-# In[9]:
-
-
 #hvplot.help('scatter')
+
+
+# In[ ]:
+
+
+
 
