0faa6f6b315cb5f2842f13513291e4c233e2bfb0
Willy Stöckel <wilsto2@web.de>
2021-06-24 16:56:42 +0200

    added time axe, plot direct to html

--- book/_build/jupyter_execute/radar_wind_bars.py
+++ book/_build/jupyter_execute/radar_wind_bars.py
@@ -9,14 +9,14 @@
 
 
 import xarray as xr
+#import holoviews.plotting.mpl
+import pandas as pd
 import matplotlib.pyplot as plt
 import panel as pn
 import hvplot.xarray
 import glob
 import param
 import numpy as np
-#from bokeh.plotting import figure, show, output_notebook
-#output_notebook()
 
 
 # In[2]:
@@ -28,42 +28,36 @@ root_path = '/project/MA_vis/MA_visualization/data/'
 # In[3]:
 
 
-root_path = '/project/MA_vis/MA_visualization/data/'
 cascade_infiles = sorted(glob.glob(f'{root_path}*.h5'))
 ds_wind = xr.open_mfdataset(cascade_infiles[0:11], concat_dim=['phony_dim_8'],group ='wind', combine = 'nested', engine='netcdf4')
 ds_info = xr.open_dataset(cascade_infiles[0], group = 'info',  engine='netcdf4')
-dates=[]
-for i in cascade_infiles:
-    dates.append(i[-13:-3])
-
+ds_info2 = xr.open_dataset(cascade_infiles[-1], group = 'info',  engine='netcdf4')
 ds_wind = ds_wind.rename({'phony_dim_8': 'time', 'phony_dim_9': 'alt'})
 ds_wind['alt'] = ds_info['alt'].squeeze().values
 ds_wind['alt'].attrs['long_name'] = 'altitude'
 ds_wind['alt'].attrs['units'] = 'km'
-ds_wind['time']=np.arange(0,11,1/24)
-ds_wind['time'].attrs['units'] = f'days after {dates[0]}'
+restr = lambda x: int(x[0])
+year, month, day = map(restr, ds_info['date'].values)
+year2, month2, day2 = map(restr, ds_info2['date'].values)
+ds_wind['time']=pd.date_range(start=f'{year}-{month:02d}-{day:02d}', end=f'{year2}-{month2:02d}-{day2:02d}', periods = (len(cascade_infiles)-1)*24)
+ds_wind['time']
 
 
 # In[4]:
 
 
 bars = ds_wind.hvplot.scatter(y=['u','v'], symmetric =True, ylim=[-100,100], groupby='alt')
+bars
 
 
 # In[5]:
 
 
-#hvplot.help('scatter')
+hvplot.save(bars, 'bars.html')
 
 
 # In[6]:
 
 
-bars
-
-
-# In[ ]:
-
-
-
+hvplot.save(bars, 'bars.html')
 
